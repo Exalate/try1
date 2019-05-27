2e4fd1c1b007782fea9c70034027a4c6b95e4392
Exalate <yaro84@mail.ru>
2019-05-27 11:44:27 +0300

    Initial commit

---
+++ src/test/java/Entrance/MainTest.java
@@ -0,0 +1,14 @@
+package Entrance;
+
+import org.junit.Test;
+
+public class MainTest extends Main {
+
+    @Test
+    public void main() {
+
+        Main main = new Main();
+        main.start();
+
+    }
+}
